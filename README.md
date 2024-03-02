f9f5908e4825ad0a6317c4989865f7c3d073715b
Arca Ege Cengiz <arcaegecengiz@gmail.com>
2024-03-02 23:35:11 +0000

    Added LoRa library and updated README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Receiver
+
+Contains the code for the ground station receiver. For the code that
+runs on the CanSat, go [here](https://github.com/CanSat-Athena/CanSat)
---
+++ ground-receiver.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#include "pico-lora/src/LoRa-RP2040.h"
+
+int main()
+{
+    stdio_init_all();
+    puts("Hello, world!");
+
+    return 0;
+}
