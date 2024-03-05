8bd9beefc0cd22c55b5f3c5889ba431e89f78f32
Arca Ege Cengiz <arcaegecengiz@gmail.com>
2024-03-05 01:13:31 +0000

    Added regular write

--- ground-receiver.cpp
+++ ground-receiver.cpp
@@ -3,41 +3,58 @@
 #include "pico-lora/src/LoRa-RP2040.h"
 
 #include "config.h"
+#include "CanSat/src/commonTypes.h"
+#include "CanSat/src/config.h"
 
 // Global vars
 const uint attempts = 3;
 
 void initLoRa();
 
-int main() {
+// for (uint8_t i = 0; i < 3; i++) {
+//     accel_g[i] = (float)accel_raw[i] / (16384.0f / 1);
+//     gyro_dps[i] = (float)gyro_raw[i] / 131.0f;
+//     mag_ut[i] = ((float)mag_raw[i] / 20) * 3;
+// }
 
+void onReceive(int packetSize) {
+    // Read type
+    // char type = LoRa.read();
+
+    // if (type != 't') putchar(type);
+
+    // Read packet
+    for (int i = 0; i < packetSize; i++) {
+        char c = LoRa.read();
+        if (i > 0)
+            printf("%c", c);
+    }
+
+    // print RSSI of packet
+    // printf("' with RSSI ");
+    // printf("%d\n", LoRa.packetRssi());
+}
+
+int main() {
     stdio_init_all();
 
     sleep_ms(1000);
-    
+
     puts("Hello, world!");
 
     // Set pins and initialise LoRa
     LoRa.setPins(RADIO_NSS_PIN, RADIO_RESET_PIN, RADIO_DIO0_PIN);
     initLoRa();
 
-    int counter = 0;
-
+    LoRa.onReceive(onReceive);
+    LoRa.receive();
     while (true) {
-        int packetSize = LoRa.parsePacket();
-        if (packetSize) {
-            // received a packet
-            printf("Received packet \n'");
-
-            // read packet
-            while (LoRa.available()) {
-                putchar((char)LoRa.read());
-            }
-
-            // print RSSI of packet
-            printf("' with RSSI \n");
-            printf((char*)LoRa.packetRssi(), "\n");
-        }
+        sleep_ms(5000);
+        printf("Writing\n");
+        LoRa.beginPacket();
+        LoRa.write((const uint8_t*)"asdf", 4);
+        LoRa.endPacket();
+        LoRa.receive();
     }
 }
 
@@ -63,6 +80,10 @@ void initLoRa() {
         }
 
         printf("Radio:      Initialised successfully\n");
+        LoRa.enableCrc();
+        LoRa.setSignalBandwidth(RADIO_BANDWIDTH);
+        LoRa.setSpreadingFactor(RADIO_SPREAD_FACTOR);
+
         return;
     }
 }
